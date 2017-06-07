f74048b1a988c013f39c92f787872f558b3a4e02
Wolfy <s.leheup@gmail.com>
2017-06-07 19:02:07 +0200

    fight vs the form

--- djo-wolfy/src/App.js
+++ djo-wolfy/src/App.js
@@ -1,21 +1,75 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    pseudo: ''
+  };
+
+  // welcomePseudo = event => {
+  //   event.preventDefault();
+  //   const pseudo = this.pseudo.value;
+  //   console.log(pseudo);
+  //   this.setState({pseudo})
+  // };
+
+  changeFirstName = (event) => {
+    const { contact } = this.state;
+    const newContact = {
+      ...contact,
+      firstName: event.target.value
+    };
+    this.setState({ contact: newContact });
+  }
+
+  changeLastName = (event) => {
+    const { contact } = this.state;
+    const newContact = {
+      ...contact,
+      lastName: event.target.value
+    };
+    this.setState({ contact: newContact });
+  }
+
+  changeEmail = (event) => {
+    const { contact } = this.state;
+    const newContact = {
+      ...contact,
+      email: event.target.value
+    };
+    this.setState({ contact: newContact });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Discover them !</h2>
+          <h3></h3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+         <input type="text" onChange={this.changeFirstName} value={this.state.contact.firstName}/>
+         <input type="text" onChange={this.changeLastName} value={this.state.contact.lastName}/>
+         <input type="text" onChange={this.changeEmail} value={this.state.contact.email}/>
+       </div>
+        <div className="App-intro">
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" required ref={input => {
+                this.pseudo = input
+              }}/>
+            </label>
+            <button type="submit">Suivant</button>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+  // firstname lastname age email location score
